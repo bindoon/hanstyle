7fdaa49198cc8f833e6a7d14aeeff08343d481d1
qianchen.qc <qianchen.qc@alibaba-inc.com>
2015-04-29 07:34:27 +0800

    菜单

--- menu.php
+++ menu.php
@@ -1,60 +1,58 @@
 access_token = "";
 
-$jsonmenu = '{
-      "button":[
-      {
-            "name":"天气预报",
-           "sub_button":[
+$jsonmenu = '
+{
+    "button":[
+    {
+        "name":"汉文化",
+            "sub_button":[
             {
-               "type":"click",
-               "name":"北京天气",
-               "key":"天气北京"
+                "type":"click",
+                "name":"测试",
+                "key":"test"
             },
             {
-               "type":"click",
-               "name":"上海天气",
-               "key":"天气上海"
-            },
-            {
-               "type":"click",
-               "name":"广州天气",
-               "key":"天气广州"
-            },
-            {
-               "type":"click",
-               "name":"深圳天气",
-               "key":"天气深圳"
+                "type":"click",
+                "name":"求签解答",
+                "key":"wantanswer"
             },
             {
                 "type":"view",
                 "name":"本地天气",
                 "url":"http://m.hao123.com/a/tianqi"
             }]
-      
-
-       },
-       {
-           "name":"方倍工作室",
-           "sub_button":[
-            {
-               "type":"click",
-               "name":"公司简介",
-               "key":"company"
-            },
-            {
-               "type":"click",
-               "name":"趣味游戏",
-               "key":"游戏"
-            },
-            {
-                "type":"click",
-                "name":"讲个笑话",
-                "key":"笑话"
-            }]
-       
-
-       }]
- }';
+    },
+    {
+        "name":"汉style",
+        "sub_button":[
+        {
+            "type":"click",
+            "name":"茶道",
+            "key":"tea"
+        },
+        {
+            "type":"click",
+            "name":"能量",
+            "key":"energy"
+        }]
+    },
+    {
+        "name":"我",
+        "sub_button":[
+        {
+            "type":"click",
+            "name":"今日运势",
+            "key":"luckday"
+        },
+        {
+            "type":"click",
+            "name":"猜你喜欢",
+            "key":"guess"
+        }]
+    }
+    ]
+}
+';
 
 
 $url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
@@ -76,21 +74,21 @@ function https_request($url,$data = null){
     return $output;
 }
 
-    function get_access_token() {
+function get_access_token() {
 
-        $appid = "";
-        $appsecret = "";
-        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret";
+    $appid = "";
+    $appsecret = "";
+    $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret";
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        $jsoninfo = json_decode($output, true);
-        $access_token = $jsoninfo["access_token"];
-        
-    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    $jsoninfo = json_decode($output, true);
+    $access_token = $jsoninfo["access_token"];
+
+}
 ?>
--- wx_cgi.php
+++ wx_cgi.php
@@ -36,7 +36,7 @@ class wechatCallbackapiTest
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $RX_TYPE = trim($postObj->MsgType);
 
-	    $result = "";
+            $result = "";
             switch ($RX_TYPE)
             {
                 case "event":
@@ -54,38 +54,10 @@ class wechatCallbackapiTest
         }
     }
 
-    private function receiveEvent($object)
-    {
-        switch ($object->Event)
-        {
-            case "subscribe":
-                $content = "欢迎关注汉时代";
-                break;
-        }
-        $result = $this->transmitText($object, $content);
-        return $result;
-    }
-
-    private function transmitText($object, $content)
-    {
-		if (!isset($content) || empty($content)){
-			return "";
-		}
-        $textTpl = "<xml>
-<ToUserName><![CDATA[%s]]></ToUserName>
-<FromUserName><![CDATA[%s]]></FromUserName>
-<CreateTime>%s</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[%s]]></Content>
-</xml>";
-        $result = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content);
-        return $result;
-    }
-
     private function receiveText($object)
     {
         $keyword = trim($object->Content);
-	$url = "http://apix.sinaapp.com/weather/?appkey=".$object->ToUserName."&city=".urlencode($keyword); 
+        $url = "http://apix.sinaapp.com/weather/?appkey=".$object->ToUserName."&city=".urlencode($keyword); 
         $output = file_get_contents($url);
         $content = json_decode($output, true);
 
@@ -93,33 +65,91 @@ class wechatCallbackapiTest
         return $result;
     }
 
+    private function receiveEvent($object)
+    {
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $contentStr = "欢迎关注汉时代!";
+                break;
+            case "unsubscribe":
+                break;
+            case "CLICK":
+                switch ($object->EventKey)
+                {
+                    case "test":
+                        $contentStr[] = array("Title" =>"公司简介", 
+                        "Description" =>"简单测试", 
+                        "PicUrl" =>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", 
+                        "Url" =>"weixin://addfriend/pondbaystudio");
+                        break;
+                    case "luckday":
+                        $contentStr = "what a luck day!";
+                        break;
+                    default:
+                        $contentStr[] = array("Title" =>"默认菜单回复", 
+                        "Description" =>"其他默认", 
+                        "PicUrl" =>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", 
+                        "Url" =>"weixin://addfriend/pondbaystudio");
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        
+        if (is_array($contentStr)){
+            $resultStr = $this->transmitNews($object, $contentStr);
+        }else{
+            $resultStr = $this->transmitText($object, $contentStr);
+        }
+        return $resultStr;
+    }
+
+    private function transmitText($object, $content)
+    {
+        if (!isset($content) || empty($content)){
+            return "";
+        }
+        $textTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[%s]]></Content>
+            </xml>";
+        $result = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content);
+        return $result;
+    }
+
+
 
     private function transmitNews($object, $newsArray)
     {
+        //首条标题28字，其他标题39字
         if(!is_array($newsArray)){
             return "";
         }
         $itemTpl = "    <item>
-        <Title><![CDATA[%s]]></Title>
-        <Description><![CDATA[%s]]></Description>
-        <PicUrl><![CDATA[%s]]></PicUrl>
-        <Url><![CDATA[%s]]></Url>
-    </item>
-";
+            <Title><![CDATA[%s]]></Title>
+            <Description><![CDATA[%s]]></Description>
+            <PicUrl><![CDATA[%s]]></PicUrl>
+            <Url><![CDATA[%s]]></Url>
+            </item>
+            ";
         $item_str = "";
         foreach ($newsArray as $item){
             $item_str .= sprintf($itemTpl, $item['Title'], $item['Description'], $item['PicUrl'], $item['Url']);
         }
         $newsTpl = "<xml>
-<ToUserName><![CDATA[%s]]></ToUserName>
-<FromUserName><![CDATA[%s]]></FromUserName>
-<CreateTime>%s</CreateTime>
-<MsgType><![CDATA[news]]></MsgType>
-<Content><![CDATA[]]></Content>
-<ArticleCount>%s</ArticleCount>
-<Articles>
-$item_str</Articles>
-</xml>";
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[news]]></MsgType>
+            <Content><![CDATA[]]></Content>
+            <ArticleCount>%s</ArticleCount>
+            <Articles>$item_str</Articles>
+            </xml>";
 
         $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count($newsArray));
         return $result;
@@ -127,7 +157,7 @@ $item_str</Articles>
 
     private function logger($log_content)
     {
-      
+
     }
 }
 ?>
