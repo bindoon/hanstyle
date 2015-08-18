d7853bea7539424725979b4162d362f5d830b8e4
bindoon <bindoon@sina.com>
2015-08-19 07:23:46 +0800

    h5页面调整

--- app/controllers/article-controller.js
+++ app/controllers/article-controller.js
@@ -7,11 +7,23 @@ exports.getArticle = function(req, res, next) {
 
     co(function* (){
         
-        return yield dataHandle.getArticleById(articleid);
+        var article =  yield dataHandle.getArticleById(articleid);
+        if(article) {
+            article = article[0];
+            var expert = yield dataHandle.getExpert(article.expertId);
+            if (expert) {
+                expert = expert[0];
+                var category  = yield dataHandle.getCategory(expert.cateId);
+                if(category) {
+                    article.typename = category[0].name
+                }
+            };
+        }
+        return article;
 
     }).then(function(data){
         if(data)
-        res.render("article",data[0]);
+        res.render("article",data);
         else res.render("error",{});
     });
 
